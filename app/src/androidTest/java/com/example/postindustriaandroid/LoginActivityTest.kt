5e02f3df9102b5fa9619c61aaab87c4308c233d5
andrewkakul <a.kuleshov.igorevich@gmail.com>
2021-01-27 13:59:05 +0300

    week-9

--- app/src/androidTest/java/com/example/postindustriaandroid/LoginActivityTest.kt
+++ app/src/androidTest/java/com/example/postindustriaandroid/LoginActivityTest.kt
@@ -1,17 +1,22 @@
 package com.example.postindustriaandroid
 
+import android.app.Activity
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.runner.lifecycle.ActivityLifecycleMonitorRegistry
+import androidx.test.runner.lifecycle.Stage
+import com.example.postindustriaandroid.ui.BaseActivity
 import com.example.postindustriaandroid.ui.LogInActivity
+import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.junit.Before
 
 @RunWith(AndroidJUnit4::class)
 class LoginActivityTest {
@@ -30,6 +35,7 @@ class LoginActivityTest {
             .check(ViewAssertions.matches(isDisplayed()))
         onView(withId(R.id.loIn_ET)).perform(typeText(login))
         onView(withId(R.id.logIn_btn)).perform(click())
+        assertEquals(getActivityInstance()!!.javaClass, BaseActivity::class.java)
     }
 
     @Test
@@ -37,5 +43,18 @@ class LoginActivityTest {
         onView(withId(R.id.login_layout))
             .check(ViewAssertions.matches(isDisplayed()))
         onView(withId(R.id.logIn_btn)).perform(click())
+        assertEquals(getActivityInstance()!!.javaClass, LogInActivity::class.java)
+    }
+
+    private fun getActivityInstance(): Activity? {
+        val currentActivity = arrayOf<Activity?>(null)
+        InstrumentationRegistry.getInstrumentation().runOnMainSync {
+            val resumedActivities: Collection<*> =
+                ActivityLifecycleMonitorRegistry.getInstance().getActivitiesInStage(Stage.RESUMED)
+            if (resumedActivities.iterator().hasNext()) {
+                currentActivity[0] = resumedActivities.iterator().next() as Activity?
+            }
+        }
+        return currentActivity[0]
     }
 }
